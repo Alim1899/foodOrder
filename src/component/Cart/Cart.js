0b9f506c0a527102ae83cf6def6315b298dc0481
Alim1899 <alimakhchanogli@gmail.com>
2023-04-27 00:45:02 +0400

    Added Modal Overlay

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
 const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
@@ -11,17 +12,17 @@ const Cart = (props) => {
     </ul>
   );
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>34.43</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
-        <button className={classes.button}></button>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
