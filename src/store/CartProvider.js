0de78d9742b58f6ef4bf664eb6e6664bc5d7589d
Alim1899 <alimakhchanogli@gmail.com>
2023-04-27 16:22:35 +0400

    Added more complex reducer logic

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,9 +8,29 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const exiStingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartIntem = state.items[exiStingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartIntem) {
+      const updatedItem = {
+        ...existingCartIntem,
+        amount: existingCartIntem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[exiStingCartItemIndex] = updatedItem;
+    } else {
+      
+      updatedItems = state.items.concat(action.item);
+    }
+
+    //const updatedItems = state.items.concat(action.item);
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
